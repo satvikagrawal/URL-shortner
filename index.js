66552c7dbdedb20f047c04695e6b2c4d0e378c9f
satvikagrawal <91125431+satvikagrawal@users.noreply.github.com>
2024-03-01 14:14:10 +0530

    deployed on github
    major fixes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const{connectToMongoDB} = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require('./models/url');
+const app = express();
+const PORT =8001;
+ connectToMongoDB("mongodb://localhost:27017/short-url")
+ .then(() => console.log('Mongodb connected'))
+ app.use(express.json());
+app.use("/url", urlRoute);
+
+app.get('/:shortid', async (req, res)=>{
+   const shortid =req.params.shortid;
+   const entry = await URL.findOneAndUpdate({
+      shortid
+   }, {
+    $push:{
+    visithistory:{ 
+        timestamp: Date.now(),
+    }
+   }, });
+   res.redirect(entry.redirectURL);
+})
+app.listen(PORT, ()=> console.log(`Server started at port:${PORT} `)) 
---
+++ models/url.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+    shortid:{
+        type: String,
+        required : true,
+        unique: true,
+    },
+    redirectURL:{
+        type: String,
+        required: true,
+    },
+    visithistory: [{timestamp: {type: Number}}],
+},
+ { timestamps: true}
+);
+
+const URL = mongoose.model('url', urlSchema);
+
+module.exports = URL;
